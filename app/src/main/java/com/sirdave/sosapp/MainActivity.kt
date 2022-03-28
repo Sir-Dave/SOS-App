12123c529c50acc73a11541986654092c77603c6
Abiola David <davidolanrewaju2000@gmail.com>
2022-03-28 21:35:54 +0100

    Set up UI for MainActivity

--- app/src/main/java/com/sirdave/sosapp/MainActivity.kt
+++ app/src/main/java/com/sirdave/sosapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.sirdave.sosapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.sirdave.sosapp.contact.ContactFragment
+import com.sirdave.sosapp.contact_list.ContactListFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addNewContact : Button = findViewById(R.id.addContact)
+        val displayContact: Button = findViewById(R.id.displayContact)
+
+        val contactFragment = ContactFragment()
+        val contactListFragment = ContactListFragment()
+
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+
+        addNewContact.setOnClickListener {
+            transaction.add(R.id.container_home, contactFragment, "contactFragment")
+            transaction.commit()
+        }
+
+        displayContact.setOnClickListener {
+            transaction.add(R.id.container_home, contactListFragment, "contactListFragment")
+            transaction.commit()
+        }
     }
 }
