2eac5bd675d1b960837be36901b9c9c93825feff
Abiola David <davidolanrewaju2000@gmail.com>
2022-03-30 13:13:06 +0100

    Implemented UI to capture image from camera

--- app/src/main/java/com/sirdave/sosapp/ui/CameraFragment.kt
+++ app/src/main/java/com/sirdave/sosapp/ui/CameraFragment.kt
@@ -2,9 +2,12 @@ package com.sirdave.sosapp.ui
 
 import android.os.Bundle
 import android.util.Base64
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import com.otaliastudios.cameraview.CameraListener
 import com.otaliastudios.cameraview.CameraView
@@ -22,16 +25,21 @@ class CameraFragment : Fragment() {
 
         cameraView = view.findViewById(R.id.cameraView)
         cameraView.setLifecycleOwner(viewLifecycleOwner)
+        val button: ImageButton = view.findViewById(R.id.captureImage)
 
         cameraView.addCameraListener(object : CameraListener(){
             override fun onPictureTaken(result: PictureResult) {
                 // Access the raw data if needed.
                 val byteArrayImage = result.data
                 val encodedImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT)
+                Log.d("CameraFragment", "encodedImage is $encodedImage")
 
             }
         })
 
+        button.setOnClickListener {
+            cameraView.takePicture()
+        }
 
         return view
     }
