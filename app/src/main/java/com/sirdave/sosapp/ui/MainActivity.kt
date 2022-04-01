023c33f33de1aafb450de1f20c64aea3f820b0a0
Abiola David <davidolanrewaju2000@gmail.com>
2022-04-01 02:21:59 +0100

    Refactored UI to use bottomNavigation

--- app/src/main/java/com/sirdave/sosapp/ui/MainActivity.kt
+++ app/src/main/java/com/sirdave/sosapp/ui/MainActivity.kt
@@ -1,20 +1,56 @@
 package com.sirdave.sosapp.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sirdave.sosapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var bottomNavView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavView = findViewById(R.id.bottom_nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        setupActionBarWithNavController(navController)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_camera, R.id.navigation_contact
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavView.setupWithNavController(navController)
+        setupBottomNav(navController)
+    }
+
+    private fun setupBottomNav(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.navigation_home -> showBottomNav()
+                R.id.navigation_camera -> showBottomNav()
+                R.id.navigation_contact -> showBottomNav()
+                else -> hideBottomNav()
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+        bottomNavView.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        bottomNavView.visibility = View.GONE
     }
 }
--- app/src/main/java/com/sirdave/sosapp/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/sirdave/sosapp/ui/fragments/HomeFragment.kt
@@ -5,9 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import androidx.navigation.Navigation
 import com.sirdave.sosapp.R
 
 class HomeFragment : Fragment() {
@@ -16,25 +13,6 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-
-        val addNewContact : Button = view.findViewById(R.id.addContact)
-        val displayContact: Button = view.findViewById(R.id.displayContact)
-        val imageView: ImageView = view.findViewById(R.id.imageView)
-
-        imageView.setOnClickListener{
-            val navController = Navigation.findNavController(it)
-            navController.navigate(R.id.openCamera)
-        }
-
-        addNewContact.setOnClickListener {
-            val navController = Navigation.findNavController(it)
-            navController.navigate(R.id.newContact)
-        }
-
-        displayContact.setOnClickListener {
-            val navController = Navigation.findNavController(it)
-            navController.navigate(R.id.viewContactList)
-        }
 
         return view
     }
